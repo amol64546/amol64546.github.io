ff90856ad0e95d057fe19060716313d6a3935b02
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2022-12-23 12:03:29 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+ function light(alt){
+        var pic;
+        if (alt==0) {
+            pic=""
+        }
+        else {
+            pic="https://i.ytimg.com/vi/AFeoFr3aXFE/hqdefault.jpg"
+        }
+        document.getElementById('img').src = pic;
+    }
